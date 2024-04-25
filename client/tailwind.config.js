d28fcd98635ad305cc89091a1270f04282796955
vagabondachi <pearlypingsonbruno@gmail.com>
2024-04-25 12:13:32 +0800

    tailwind test

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/lib/esm/**/*.js'
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary:  ['Bantayog', 'sans'],
+        secondary: ['Baloo', 'sans'],
+        secondaryRegular: ['Baloo2-Regular', 'sans'],
+        secondarySemiBold: ['Baloo2-SemiBold', 'sans']
+      },
+      fontSize: {
+        sm: '18px', 
+      },
+      colors: {
+        textMain: '#777777',
+        textLight: '#AFAFAF',
+        textwhite: '#F7F7F7',
+        primary: '#56CD02',
+        secondary: '#89E219',
+        accentYellow: '#FFC800',
+        accentBlue: '#2B70C9',
+        accentLightBlue: '#1CB0F6',
+        accentPurple: '#CE82FF',
+        accentRed: '#FF4B4B',
+        accentWhiteGray: '#E5E5E5',
+        accentGray: '#AFAFAF'
+      },
+
+    },
   },
-  plugins: [],
+  
+  plugins: [
+    require('flowbite/plugin')
+  ]
 }
 
